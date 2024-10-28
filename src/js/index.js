f06f23de4417b2b1f241120e4e93b4bcc2edb799
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-10-28 17:41:52 +0300

    Добавлено согласие на сбор cookie

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import selectFilter from "./modules/selectFilter.js";
 import initializePreloader from "./modules/preloader.js";
 import mobileMenu from "./modules/mobileMenu.js";
 import animationSvgToScroll from "./modules/animationSvgToScroll.js";
-import startSlideShow from "./modules/slider.js"; 
+import startSlideShow from "./modules/slider.js";
 import initCounterObserver from "./modules/counterNumber.js";
 
 initializePreloader();
@@ -14,4 +14,34 @@ animationSvgToScroll();
 startSlideShow();
 initCounterObserver();
 
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + encodeURIComponent(value || "") + expires + "; path=/";
+}
 
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var consent = getCookie("cookieConsentAccepted");
+  if (!consent) {
+    document.getElementById("cookieConsent").style.display = "flex";
+
+    document.getElementById("acceptCookie").addEventListener("click", function() {
+      setCookie("cookieConsentAccepted", "true", 365);
+      document.getElementById("cookieConsent").style.display = "none";
+    });
+  }
+});
